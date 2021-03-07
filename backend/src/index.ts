7cbb60ab9f4dbde76b2331f35b523055c79ebd5d
nkuek <nkuek1@gmail.com>
2021-03-07 02:46:02 -0500

    attempting to set up apollo

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import "dotenv/config"
+import "#root/db/connection"
+// import "#root/server/startServer"
+// import express from 'express'
+// import routes from './routes'
+
+
+// const app = express();
+// app.use(express.json());
+// app.use(routes);
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World')
+// })
+
+// const port = 5000
+// app.listen(port, () => {
+//     console.log(`Listening on port ${port}...`)
+// })
+// export default app
