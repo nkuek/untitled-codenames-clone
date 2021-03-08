6ea7f4cb81dc85322bf311074ebdffa4451a8fda
nkuek <nkuek1@gmail.com>
2021-03-07 23:33:49 -0500

    comments to test component

--- frontend/src/components/Testing.tsx
+++ frontend/src/components/Testing.tsx
@@ -17,10 +17,20 @@ const query = gql`
     }`
 
 const Testing = () => {
-    const {data, loading} = useQuery<QueryData>(query)
-    const results = (data?.smallWordList)
-    const words = results?.map(result => result.word)
 
+    // Data = object containing result of query
+    // loading = boolean indicating if query is still begin executed
+    const {data, loading} = useQuery<QueryData>(query)
+
+    // data is in a strange format. unsure how to format it on the backend.
+    const results = (data?.smallWordList)
+    console.log(results)
+
+    // format data into individual words
+    const words = results?.map(result => result.word)
+    console.log(words)
+
+    // conditionally render testing component if the query is no longer loading
     return (
         loading ? <h1>Loading...</h1> :
         <>
