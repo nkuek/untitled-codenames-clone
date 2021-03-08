88f89ecab3a5adab2b34b7ab98895ae42f6eb2fe
nkuek <nkuek1@gmail.com>
2021-03-07 19:54:47 -0500

    started integrating apollo into frontend and added comments to the backend

---
+++ frontend/src/api/graphql.ts
@@ -0,0 +1,10 @@
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import {InMemoryCache} from 'apollo-cache-inmemory'
+
+export const cache = new InMemoryCache
+
+const client = new ApolloClient({
+    cache,
+    link: new HttpLink({uri: process.env.SERVICES_URI + "/graphql"})
+})
